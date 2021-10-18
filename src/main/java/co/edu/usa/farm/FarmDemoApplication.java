d8894ca3e605bd8a85193373782abaaa03adb727
Jorge Prieto <jprietof@outlook.es>
2021-10-17 23:33:18 -0500

    Creación de la estructura de sring boot y configuración de application properties

---
+++ src/main/java/co/edu/usa/farm/FarmDemoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usa.farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/usa/farm/FarmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
