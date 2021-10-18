9f069a220c436b12f0820139f8e8cd798a72c075
Jorge Prieto <jprietof@outlook.es>
2021-10-18 16:23:59 -0500

    Se creo 4 clases y 1 interface de Reservacion

--- src/main/java/co/edu/usa/farm/model/Farm.java
+++ src/main/java/co/edu/usa/farm/model/Farm.java
@@ -1,16 +1,42 @@
 package co.edu.usa.farm.model;
 
+import java.util.*;
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "farm")
 public class Farm implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String address;
+	private Integer exension;
+	private String name;
+	private String description;
 	
-
+	@ManyToOne
+	@JoinColumn(name="categoryId")
+	@JsonIgnoreProperties("farm")
+	private Category category;
+	
+	@OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "farm")
+	@JsonIgnoreProperties({"farm", "client"})
+	private List<Message> messages;
+	
+	@OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "farm")
+	@JsonIgnoreProperties({"farm", "client"})
+	private List<Reservation> reservations;
+	
+	
 }
