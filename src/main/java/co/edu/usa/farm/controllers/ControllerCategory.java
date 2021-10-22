0804b5f0f39a66b44dea158accd022bb0ca265b3
Jorge Prieto <jprietof@outlook.es>
2021-10-22 06:55:02 -0500

    Se agregaron save y delete al controlador de categorias

--- src/main/java/co/edu/usa/farm/controllers/ControllerCategory.java
+++ src/main/java/co/edu/usa/farm/controllers/ControllerCategory.java
@@ -4,11 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.usa.farm.model.Category;
@@ -29,6 +35,23 @@ public class ControllerCategory {
 	public Optional<Category> getCategory(@PathVariable("id") int categoryId){
 		return service.getCategory(categoryId);
 	}
+	
+	@PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category save(@RequestBody Category category) {
+        return service.save(category);
+    }
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category update(@RequestBody Category category) {
+        return service.update(category);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id") int categoryId) {
+        return service.deleteCategory(categoryId);
+    }
 	
 	
 }
